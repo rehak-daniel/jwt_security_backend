cd7494345ed3e61fd82685f4c7208777c76daeee
rehak-daniel <rehak.daniel17@gmail.com>
2026-08-25 15:03:51 +0200

    create new spring boot project

---
+++ src/main/java/com/securityproject/JwtSecurityBackendApplication.java
@@ -0,0 +1,13 @@
+package com.securityproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityBackendApplication.class, args);
+	}
+
+}
